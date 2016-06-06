d96b6f8dde21b4a4d55ae78f111f5d95011fa30b
Jeff Hackford and Ryan Harvey <ryan@>
2016-06-06 10:39:11 -0700

    add show and delete actions to each listing

---
+++ app/components/rental-tile.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  isImageShowing: false,
+  actions: {
+    imageShow: function() {
+      this.set('isImageShowing', true);
+    },
+    imageHide: function() {
+      this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
+    }
+  }
+});
