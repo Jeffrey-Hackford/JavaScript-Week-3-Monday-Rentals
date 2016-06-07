70a264a5f8707cbbebb2ed15448c6a54ae2445ff
Jacob Hefley and Jeffrey Hackford <jeffrey@>
2016-06-07 11:16:34 -0700

    dry up the code and add id functionality

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,14 +8,6 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
-    },
-    delete(rental) {
-      if (confirm('are you sure you want to delete this rental?')) {
-        this.sendAction('destroyRental', rental);
-      }
-    },
-    update(rental, params) {
-        this.sendAction('update', rental, params);
-      }
     }
+  }
 });
